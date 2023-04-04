39c00b9206301605179f4ee5f00a043f712d85b5
weicheng95 <weicheng0324094@gmail.com>
2023-04-04 17:51:27 +0800

    add support for flags

--- README.md
+++ README.md
@@ -1,3 +1,42 @@
-## CLI Translation Tool powered by OpenAI GPT-3.5
+# CLI Translation Tool
 
-This tool allows users to translate their text-based inputs from one language to another through a command-line interface.
+This CLI Translation Tool is a command-line interface that allows users to translate text from one language to another using the OpenAI GPT-3.5 API. The tool takes in text input in one language and outputs the translation in another language through the command line.
+
+## Prerequisites
+
+Before running this tool, we recommend you install the following dependencies:
+
+- Deno
+- OpenAI GPT-3.5 API Key
+
+## Installation
+
+1. Clone the repository.
+   `git clone https://github.com/sonderbase/gpt-translator.git`
+2. Navigate to the project directory.
+   `cd gpt-translator`
+3. Create a .env file and add your OpenAI API credentials.
+
+```
+OPENAI_API_KEY=<api-key>
+OPENAI_ORGANIZATION=<organization-id>
+OPENAI_MODEL=<gpt-model> // gpt-3.5-turbo
+```
+
+4. Run the CLI.
+   `deno run --allow-read --allow-env --allow-net --allow-write src/mod.ts --input=<path-to-input-file> --output=<path-to-output-file> --lang=<output-language>`
+
+## Usage
+
+The CLI Translation Tool takes in the following arguments:
+
+- --input: the path to the input file with the text to be translated.
+- --output: the path to the output file where the translated text will be written.
+- --lang: the language code for the output language (e.g. es for Spanish, fr for French).
+
+Here's an example command to translate text from English to Spanish:
+
+```deno run --allow-read --allow-env --allow-net --allow-write src/mod.ts --input=./test-data/en.json --output=./test-data/es.json --lang=es```
+
+### License
+  This project is licensed under the MIT License. Have fun experimenting with the CLI Translation Tool!
--- src/mod.ts
+++ src/mod.ts
@@ -1,22 +1,58 @@
 import { readInputFile, saveFile } from "@src/file-parser/mod.ts";
 import { translateCompletion } from "./openai/mod.ts";
 import { green, yellow } from "fmt/colors.ts";
+import { parse } from "flags";
+import * as log from "log";
+import iso639 from "iso-639-1";
+
+/**
+ * @param {string} input - input file path
+ * @param {string} output - output file path
+ * @param {string} lang - language code
+ * all these flags are required
+ */
+const flags = parse(Deno.args, {
+  string: ["input", "output", "lang"],
+});
+
+if (!flags.input) {
+  log.error("Please provide input file path");
+  Deno.exit();
+}
+
+if (!flags.output) {
+  log.error("Please provide output file path");
+  Deno.exit();
+}
+
+if (!flags.lang) {
+  log.error("Please provide language code");
+  Deno.exit();
+}
+
+// validate iso code
+if (!iso639.validate(flags.lang)) {
+  log.error("Please provide valid language code");
+  Deno.exit();
+}
 
 try {
-  const result = await readInputFile("./test-data/en.json");
+  const result = await readInputFile(flags.input);
 
-  const translateResult = await translateCompletion(result, "es");
+  log.debug(JSON.stringify(result));
+  log.info("Translating file...");
+  const translateResult = await translateCompletion(result, flags.lang);
 
-  console.log(
+  log.info(
     yellow(
-      `Prompt Tokens: ${translateResult?.usage?.prompt_tokens} | Completion Tokens: ${translateResult?.usage?.completion_tokens} | Total Tokens: ${translateResult?.usage?.total_tokens} | `
+      `Usage: Prompt Tokens: ${translateResult?.usage?.prompt_tokens} | Completion Tokens: ${translateResult?.usage?.completion_tokens} | Total Tokens: ${translateResult?.usage?.total_tokens} | `
     )
   );
 
   // save file to output folder
-  await saveFile("./translate-output/es.json", translateResult?.content || "");
-  console.log(green(`Translation completed, file save into translate-output/es.json`));
+  await saveFile(flags.output, translateResult?.content || "");
+  log.info(green(`Translation completed, file save into ${flags.output}`));
 } catch (err) {
-  console.log(err);
-  throw err;
+  log.error(err);
+  Deno.exit();
 }
